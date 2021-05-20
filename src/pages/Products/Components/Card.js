c0ade38621efa00ad250a827ba1fd2ac8bd48bf3
geborenik <geborenik@geborenikui-MacBookAir.local>
2021-05-20 23:47:00 +0900

    일부 수정

--- src/pages/Products/Components/Card.js
+++ src/pages/Products/Components/Card.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { exchangeCurrency } from '../../../utilityFunc';
 import './Card.scss';
+import { PRODUCT_API } from '../../../config';
 
 class Card extends Component {
   thisProductLike = e => {
