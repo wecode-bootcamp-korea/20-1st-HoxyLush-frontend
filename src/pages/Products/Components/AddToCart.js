23f75d2476f809fa0f1f10472593d9f76b7cc8c8
Alice Kim <alice.inkorea.frontend@gmail.com>
2021-05-20 20:47:45 +0900

    merging hell

--- src/pages/Products/Components/AddToCart.js
+++ src/pages/Products/Components/AddToCart.js
@@ -99,8 +99,8 @@ export default class AddToCart extends Component {
           />
         </div>
 
-        {isModalAlertOpen && (
-          <Modal onClose={toggleModalAlert}>
+        {/* {isModalAlertOpen && (
+          <Modal onClose={toggleModalAlert} Type={MODAL_TYPE}>
             <div className="outOfStockModal">
               <i
                 className="fas fa-times modalClose"
@@ -120,8 +120,10 @@ export default class AddToCart extends Component {
               </button>
             </div>
           </Modal>
-        )}
+        )} */}
       </div>
     );
   }
 }
+
+const MODAL_TYPE = 'AddToCart';
